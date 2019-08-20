43bda30dddd3476210f9b165e11fa85cc7179524
Mark Johnson <54310006+G4ZRT@users.noreply.github.com>
2019-08-20 22:21:50 +0100

    Update detect_platform.sh

--- detect_platform.sh
+++ detect_platform.sh
@@ -1,15 +1,26 @@
 Platform=unknown
 
+echo .......................
+echo Checking which platform
+echo 
 if grep -q "jetson-nano" /proc/device-tree/model;
 then
-echo I run on Nano
+echo I run on Nano pre 4.2.1
+echo .......................
 Platform=nano
 fi
 
 if grep -q "Raspberry" /proc/device-tree/model;
 then
 echo I run on Raspberry Pi
+echo .....................
 Platform=rpi
 fi
 
+if grep -q "jetson-nano" /proc/device-tree/model;
+then
+echo I run on Nano 4.2.1 +
+echo .....................
+Platform=nano
+fi
 
