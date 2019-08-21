109a7db7eeb22c41a44490fc99570c2bccc00622
Mark Johnson <54310006+G4ZRT@users.noreply.github.com>
2019-08-21 07:50:02 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,22 @@
 # Check running on Nano
 source ./detect_platform.sh
+echo .......................
+echo Checking which platform
+echo 
+
+if grep -q "jetson-nano" /proc/device-tree/model;
+then
+echo I run on Nano pre 4.2.1
+echo .......................
+Platform=nano
+fi
+
+if grep -q "jetson-nano" /proc/device-tree/model;
+then
+echo I run on Nano 4.2.1 +
+echo .....................
+Platform=nano
+fi
 
 if  [ "$Platform" = "nano" ] ; then
 
